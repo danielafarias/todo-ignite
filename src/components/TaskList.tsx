684e8bfd7bb2a71b406e2f82abca4315ba384cd5
Daniela Farias <farias.danicris@gmail.com>
2022-04-27 09:46:43 -0300

    Criando uma nova tarefa; Lidando com alteração da propriedade isComplete na tarefa;

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,11 +15,23 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle) {
+      const id = Math.floor(Math.random() * 100);
+      const task = {
+        id: id,
+        title: newTaskTitle,
+        isComplete: false
+      };
+      const newTasks = [...tasks, task];
+      setTasks(newTasks);
+    } else {
+      alert("O título não pode ser vazio");
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {...task, isComplete: !task.isComplete} : task);
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
